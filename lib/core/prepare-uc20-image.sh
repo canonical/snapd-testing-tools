b8e9d733496bae9df3d65bb6387b42e2168cf480
Philip Meulengracht <phailmollen@gmail.com>
2021-11-18 10:03:30 +0100

    core: add shared shell scripts for building image and controlling the L2 VM

---
+++ lib/core/prepare-uc20-image.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+set -e
+set -x 
+
+apt update -yqq
+
+# needed for dracut which is a build-dep of ubuntu-core-initramfs
+# and for the version of snapd here which we want to use to pull snap-bootstrap
+# from when we build the debian package
+add-apt-repository ppa:snappy-dev/image -y
+apt update
+
+# these should already be installed in GCE and LXD images with the google/lxd-nested 
+# backend, but in qemu local images from qemu-nested, we might not have them
+if [ "${SPREAD_BACKEND}" = "qemu-nested" ]; then
+    apt install snapd ovmf qemu-system-x86 sshpass whois -yqq
+fi
+
+# use the snapd snap explicitly
+# TODO: since ubuntu-image ships it's own version of `snap prepare-image`, 
+# should we instead install beta/edge snapd here and point ubuntu-image to this
+# version of snapd?
+snap install snapd
+
+# install some dependencies
+snap install ubuntu-image --classic
+
+# install build-deps for ubuntu-core-initramfs
+(
+    cd "$SETUPDIR"
+    apt -y build-dep ./
+)
+
+# TODO: is this still necessary for core-initrd? (this was copied from snapd)
+# create test user for spread to use
+groupadd --gid 12345 test
+adduser --uid 12345 --gid 12345 --disabled-password --gecos '' test
+
+if getent group systemd-journal >/dev/null; then
+    usermod -G systemd-journal -a test
+    id test | MATCH systemd-journal
+fi
+
+echo 'test ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+# get the model
+# TODO: should this be a variant to test multiple grades i.e. signed and secured?
+curl -o ubuntu-core-20-amd64-dangerous.model https://raw.githubusercontent.com/snapcore/models/master/ubuntu-core-20-amd64-dangerous.model
+
+# build the debian package here of ubuntu-core-initramfs
+(
+    cd "$SETUPDIR"
+    DEB_BUILD_OPTIONS='nocheck testkeys' dpkg-buildpackage -tc -b -Zgzip
+
+    # save our debs somewhere safe
+    cp ../*.deb "$SETUPDIR"
+)
+
+# install ubuntu-core-initramfs here so the repack-kernel.sh uses the version of
+# ubuntu-core-initramfs we built here
+apt install -yqq "$SETUPDIR"/ubuntu-core-initramfs*.deb
+
+# now download snap dependencies 
+# TODO:UC20: when should some of these things start tracking stable ?
+snap download pc-kernel --channel=20/$MODEL_BRANCH --basename=upstream-pc-kernel
+snap download pc --channel=20/$MODEL_BRANCH --basename=upstream-pc-gadget
+snap download core20 --channel=$MODEL_BRANCH --basename=upstream-core20
+
+# note that we currently use snap-bootstrap from the snapd deb package, but we 
+# could instead copy a potentially more up-to-date version out of the snapd snap
+# but we don't currently do that as it doesn't represent what 
+# ubuntu-core-initramfs would actually used if that package was built from LP
+snap download snapd --channel=$MODEL_BRANCH --basename=upstream-snapd
+
+# next repack / modify the snaps we use in the image, we do this for a few 
+# reasons:
+# 1. we need an automated way to get a user on the system to ssh into regardless
+#    of grade, so we add a systemd service to the snapd snap which will create 
+#    our user we can ssh into the VM with (if we only wanted to test dangerous,
+#    we could use cloud-init).
+# 2. we need to modify the initramfs in the kernel snap to use our initramfs, 
+#    which requires re-signing of the kernel.efi
+# 3. since we re-sign the kernel.efi of the kernel snap, for successful secure
+#    boot, we need to also re-sign the gadget shim too
+
+# first re-pack snapd snap with special systemd service which runs during run 
+# mode to create a user for us to inspect the system state
+# we also extract the snap-bootstrap binary for insertion into the kernel snap's
+# initramfs too
+
+snapddir=/tmp/snapd-workdir
+unsquashfs -d $snapddir upstream-snapd.snap
+
+# inject systemd service to setup users and other tweaks for us
+# these are copied from upstream snapd prepare.sh, slightly modified to not 
+# extract snapd spread data from ubuntu-seed as we don't need all that here
+cat > "$snapddir/lib/systemd/system/snapd.spread-tests-run-mode-tweaks.service" <<'EOF'
+[Unit]
+Description=Tweaks to run mode for spread tests
+Before=snapd.service
+Documentation=man:snap(1)
+
+[Service]
+Type=oneshot
+ExecStart=/usr/lib/snapd/snapd.spread-tests-run-mode-tweaks.sh
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > "$snapddir/usr/lib/snapd/snapd.spread-tests-run-mode-tweaks.sh" <<'EOF'
+#!/bin/sh
+set -e
+# Print to kmsg and console
+# $1: string to print
+print_system()
+{
+    printf "%s spread-tests-run-mode-tweaks.sh: %s\n" "$(date -Iseconds --utc)" "$1" |
+        tee -a /dev/kmsg /dev/console /run/mnt/ubuntu-seed/spread-tests-run-mode-tweaks-log.txt || true
+}
+
+# ensure we don't enable ssh in install mode or spread will get confused
+if ! grep 'snapd_recovery_mode=run' /proc/cmdline; then
+    print_system "not in run mode - script not running"
+    exit 0
+fi
+if [ -e /root/spread-setup-done ]; then
+    print_system "already ran, not running again"
+    exit 0
+fi
+
+print_system "in run mode, not run yet, extracting overlay data"
+
+# extract data from previous stage
+(cd / && tar xf /run/mnt/ubuntu-seed/run-mode-overlay-data.tar.gz)
+cp -r /root/test-var/lib/extrausers /var/lib
+
+# user db - it's complicated
+for f in group gshadow passwd shadow; do
+    # now bind mount read-only those passwd files on boot
+    cat >/etc/systemd/system/etc-"$f".mount <<EOF2
+[Unit]
+Description=Mount root/test-etc/$f over system etc/$f
+Before=ssh.service
+
+[Mount]
+What=/root/test-etc/$f
+Where=/etc/$f
+Type=none
+Options=bind,ro
+
+[Install]
+WantedBy=multi-user.target
+EOF2
+    systemctl enable etc-"$f".mount
+    systemctl start etc-"$f".mount
+done
+
+mkdir -p /home/test
+chown 12345:12345 /home/test
+mkdir -p /home/ubuntu
+chown 1000:1000 /home/ubuntu
+mkdir -p /etc/sudoers.d/
+echo 'test ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/99-test-user
+echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/99-ubuntu-user
+# TODO: do we need this for our nested VM? We don't login as root to the nested
+#       VM...
+sed -i 's/\#\?\(PermitRootLogin\|PasswordAuthentication\)\>.*/\1 yes/' /etc/ssh/sshd_config
+echo "MaxAuthTries 120" >> /etc/ssh/sshd_config
+grep '^PermitRootLogin yes' /etc/ssh/sshd_config
+systemctl reload ssh
+
+print_system "done setting up ssh for spread test user"
+
+touch /root/spread-setup-done
+EOF
+chmod 0755 "$snapddir/usr/lib/snapd/snapd.spread-tests-run-mode-tweaks.sh"
+
+snap pack --filename=snapd.snap "$snapddir"
+rm -r $snapddir
